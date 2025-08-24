fd9a66661ce2429391f6a360d196b3dd03a17a36
Vadim Bakhtiarov <vadima64@mail.ru>
2025-08-24 10:23:58 +0300

    Конфигурация движка и сессии для тестовой БД.

---
+++ fastapi_app/configs/a_test_session_conf.py
@@ -0,0 +1,16 @@
+"""Настройки тестового и движка и сессий."""
+
+import logging
+
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+
+from fastapi_app.configs import settings
+
+log = logging.getLogger(__name__)
+
+log.debug("Connecting to test database %s:", settings.url_test.uri)
+async_test_engine = create_async_engine(settings.url_test.uri)
+async_test_session = async_sessionmaker(
+    bind=async_test_engine,
+    expire_on_commit=False,
+)
